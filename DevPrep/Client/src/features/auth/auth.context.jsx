fc5162e3965e07f0c801721ebb04d3d74e0942f0
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-04-27 04:19:04 +0530

    Update auth.context.jsx

--- DevPrep/Client/src/features/auth/auth.context.jsx
+++ DevPrep/Client/src/features/auth/auth.context.jsx
@@ -1,25 +1,44 @@
-/** @format */
-
 import React, { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
 export const AuthContext = createContext();
 
+const API_URL =
+  "https://devprep-backend-hpnv.onrender.com/api/v1/auth";
+
 export function AuthProvider({ children }) {
-  const [accessToken, setAccessToken] = useState(null);
-  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem("accessToken") || null
+  );
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
   const [loading, setLoading] = useState(true);
 
   const refreshAccessToken = async () => {
     try {
       const response = await axios.get(
-        "https://devprep-backend-hpnv.onrender.com/api/v1/auth/refresh-token",
+        `${API_URL}/refresh-token`,
         {
           withCredentials: true, // cookie bhejne ke liye
         }
       );
-      setAccessToken(response.data.accessToken);
-      setUser(response.data.user || null);
+
+      if (response.data.accessToken) {
+        setAccessToken(response.data.accessToken);
+        localStorage.setItem(
+          "accessToken",
+          response.data.accessToken
+        );
+      }
+
+      if (response.data.user) {
+        setUser(response.data.user);
+        localStorage.setItem(
+          "user",
+          JSON.stringify(response.data.user)
+        );
+      }
     } catch (error) {
       console.log(
         "Refresh token failed:",
@@ -28,13 +47,16 @@ export function AuthProvider({ children }) {
 
       setAccessToken(null);
       setUser(null);
+
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("user");
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    refreshAccessToken(); // page reload pe chalega
+    refreshAccessToken(); 
   }, []);
 
   return (

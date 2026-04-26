a5ce1bb688e12111260d2861faafa87a94fbcd36
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-04-27 03:47:43 +0530

    Update app.js

--- DevPrep/Server/src/app.js
+++ DevPrep/Server/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json());
 app.use(cookie());
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://dev1prep.onrender.com',
     credentials:true
 }));
 app.use('/api/v1/auth',authRoutes); 

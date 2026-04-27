fc11f38d597c187a5b9e6576b4f50b68f055d7d3
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-04-27 15:10:39 +0530

    Update auth.context.jsx

--- DevPrep/Client/src/features/auth/auth.context.jsx
+++ DevPrep/Client/src/features/auth/auth.context.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
-import axios from "axios";
+import { refreshToken } from "./useAuthApi";
 
 export const AuthContext = createContext();
 
@@ -8,18 +8,13 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const api = axios.create({
-    baseURL: "https://devprep-backend-hpnv.onrender.com/api/v1",
-    withCredentials: true,
-  });
-
-  const refreshAccessToken = async () => {
+  const initAuth = async () => {
     try {
-      const res = await api.get("/auth/refresh-token");
+      const data = await refreshToken();
 
-      setAccessToken(res.data.accessToken);
-      setUser(res.data.user); 
-    } catch (err) {
+      setAccessToken(data.accessToken);
+      setUser(data.user);
+    } catch {
       setAccessToken(null);
       setUser(null);
     } finally {
@@ -28,7 +23,7 @@ export function AuthProvider({ children }) {
   };
 
   useEffect(() => {
-    refreshAccessToken();
+    initAuth();
   }, []);
 
   return (
@@ -39,7 +34,6 @@ export function AuthProvider({ children }) {
         loading,
         setAccessToken,
         setUser,
-        refreshAccessToken,
       }}
     >
       {children}

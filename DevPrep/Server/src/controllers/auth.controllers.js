50faf20f027ec7b6ebc51d4d78eb153228b4d2f7
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-04-28 17:32:18 +0530

    Update auth.controllers.js

--- DevPrep/Server/src/controllers/auth.controllers.js
+++ DevPrep/Server/src/controllers/auth.controllers.js
@@ -11,7 +11,7 @@ const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const cookieOptions = {
 	httpOnly: true,
-	secure: false, 
+	secure: true, 
 	sameSite: "none", 
 	maxAge: 7 * 24 * 60 * 60 * 1000,
 };

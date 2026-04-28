f195f9e37bff644c396b96acae1bc9e0ea80c94f
Bashar Ahmad Khan <150275763+basharahmadkhan10@users.noreply.github.com>
2026-04-28 18:15:17 +0530

    Update interview.routes.js

--- DevPrep/Server/src/routes/interview.routes.js
+++ DevPrep/Server/src/routes/interview.routes.js
@@ -1,9 +1,16 @@
-import Router  from "express";
+import Router from "express";
 import authMiddleware from "../middlewares/auth.middlewares.js";
-import {generateInterviewReportController,getInterviewReportById,getAllInterviewReport} from "../controllers/interview.controllers.js";
+import {
+	generateInterviewReportController,
+	getInterviewReportById,
+	getAllInterviewReport,
+} from "../controllers/interview.controllers.js";
 import upload from "../middlewares/file.middlewares.js";
-const router=Router();
-router.post("/report",authMiddleware,upload.single("resume"),generateInterviewReportController);
-router.get("/report/:id",authMiddleware,getInterviewReportById);
-router.get('/',authMiddleware,getAllInterviewReport);
+
+const router = Router();
+
+router.post("/report", authMiddleware, upload.single("resume"), generateInterviewReportController);
+router.get("/report/:id", authMiddleware, getInterviewReportById);
+router.get("/", authMiddleware, getAllInterviewReport);
+
 export default router;
